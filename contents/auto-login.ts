f342e76ec51e8bcb5b39b3793428ad3d1430ddf1
jingsong.xiong <jingsong.xiong@compass.com>
2024-07-24 01:13:07 +0800

    feat:finished login

---
+++ contents/auto-login.ts
@@ -0,0 +1,97 @@
+async function waitForElement(xpath, timeout = 3000) {
+  const interval = 100
+  const maxAttempts = timeout / interval
+  let attempts = 0
+
+  return new Promise((resolve, reject) => {
+    const intervalId = setInterval(() => {
+      attempts++
+      const result = document.evaluate(
+        xpath,
+        document,
+        null,
+        XPathResult.FIRST_ORDERED_NODE_TYPE,
+        null
+      )
+      const element = result.singleNodeValue
+
+      if (element) {
+        clearInterval(intervalId)
+        resolve(element)
+      } else if (attempts >= maxAttempts) {
+        clearInterval(intervalId)
+        reject(`Element not found for XPath: ${xpath} within ${timeout}ms`)
+      }
+    }, interval)
+  })
+}
+const creatLoginConfigs = (userConfigs: {
+  userName: string
+  password: string
+}) => {
+  return [
+    {
+      xpath: "/html/body/nav/nav/div[1]/div[2]/button",
+      event: "click"
+    },
+    {
+      xpath: "/html/body/div[4]/div[2]/div[2]/div/button[2]",
+      event: "click"
+    },
+    {
+      xpath: "/html/body/div[4]/div[2]/div[2]/div/button[5]",
+      event: "click"
+    },
+    {
+      xpath: "/html/body/div[4]/div[2]/div[2]/div/form/label/input",
+      event: "input",
+      value: userConfigs.userName
+    },
+    {
+      xpath: '//*[@id="continue"]',
+      event: "click"
+    },
+    {
+      xpath: "/html/body/div[4]/div[2]/div[2]/div/form/div/label/input",
+      event: "input",
+      value: userConfigs.password
+    },
+    {
+      xpath: '//*[@id="continue"]',
+      event: "click"
+    }
+  ]
+}
+
+async function executeTargets(targets) {
+  for (const target of targets) {
+    const { xpath, event, value } = target
+    try {
+      const element = await waitForElement(xpath)
+      if (event === "click") {
+        // @ts-ignore
+        element.click()
+      } else if (event === "input") {
+        console.log(value)
+        // @ts-ignore
+        element.value = value
+        // @ts-ignore
+        element.dispatchEvent(new Event("input", { bubbles: true }))
+      }
+      await new Promise((resolve) => setTimeout(resolve, 500)) // 500ms 延迟以确保事件完成
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "login") {
+    const usernameField = document.querySelector("input[name='username']")
+    const passwordField = document.querySelector("input[name='password']")
+
+    executeTargets(creatLoginConfigs({ ...request })).then(() => {
+      console.log("全部执行完毕")
+    })
+  }
+})
--- popup.tsx
+++ popup.tsx
@@ -1,26 +1,132 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import styled from "styled-components"
+import { v4 as uuidv4 } from "uuid"
+
+const defaultValues = {
+  userName: "",
+  password: ""
+}
+
+const localStorageKey = "loginConfigs"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [userNameAndPassword, setUserNameAndPassword] = useState(defaultValues)
 
+  const [error, setError] = useState<string | null>(null)
+
+  const [accountAndConfigs, setAccountAndConfigs] = useState([])
+
+  useEffect(() => {
+    setAccountAndConfigs(
+      JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    )
+  }, [])
+
+  const handleLogin = (loginConfigs: typeof userNameAndPassword) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: "login",
+        ...loginConfigs
+      })
+    })
+  }
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
-    </div>
+    <Container>
+      {error ? <div>{error}</div> : null}
+      {accountAndConfigs?.length ? (
+        accountAndConfigs.map((item) => {
+          return (
+            <UserItem id={item.userId}>
+              <div>{item.userName}</div>
+              <div>{item.password}</div>
+              <button
+                onClick={() => {
+                  const filteredConfigs = accountAndConfigs.filter(
+                    (accountConfig) => accountConfig.userId !== item.userId
+                  )
+                  localStorage.setItem(
+                    localStorageKey,
+                    JSON.stringify(filteredConfigs || "[]")
+                  )
+                  setAccountAndConfigs(filteredConfigs)
+                }}>
+                remove
+              </button>
+              <LoginButton
+                onClick={() => {
+                  handleLogin(item)
+                }}>
+                login
+              </LoginButton>
+            </UserItem>
+          )
+        })
+      ) : (
+        <div>请添加账号</div>
+      )}
+      <div>
+        <input
+          type="text"
+          onChange={(e) =>
+            setUserNameAndPassword({
+              ...userNameAndPassword,
+              userName: e.target.value
+            })
+          }
+          value={userNameAndPassword.userName}
+        />
+        <input
+          type="text"
+          onChange={(e) =>
+            setUserNameAndPassword({
+              ...userNameAndPassword,
+              password: e.target.value
+            })
+          }
+          value={userNameAndPassword.password}
+        />
+
+        <button
+          onClick={() => {
+            if (
+              userNameAndPassword.password?.length &&
+              userNameAndPassword.userName?.length
+            ) {
+              const newConfigs = JSON.parse(
+                localStorage.getItem(localStorageKey) || "[]"
+              ).concat({
+                userId: uuidv4(),
+                ...userNameAndPassword
+              })
+              localStorage.setItem(localStorageKey, JSON.stringify(newConfigs))
+              setAccountAndConfigs(newConfigs)
+              setUserNameAndPassword(defaultValues)
+            } else {
+              setError(
+                "请输入账号密码（please enter userName and Password and Password.）"
+              )
+            }
+          }}>
+          提交
+        </button>
+      </div>
+    </Container>
   )
 }
 
+const Container = styled.div`
+  width: 400px;
+  padding: 24px;
+  border-radius: 8px;
+  display: grid;
+  grid-auto-flow: row;
+  gap: 20px;
+`
+
+const UserItem = styled.div`
+  display: grid;
+  grid-template-columns: 100px 100px 70px 70px;
+  gap: 8px;
+`
+const LoginButton = styled.button``
 export default IndexPopup
