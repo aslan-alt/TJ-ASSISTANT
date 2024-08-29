b9b093d30ac15cb471e92c9af9f562717d188fab
jingsong.xiong <jingsong.xiong@compass.com>
2024-08-30 02:36:19 +0800

    Patch: add login with new tab

--- background/messages/createNewTab.ts
+++ background/messages/createNewTab.ts
@@ -5,24 +5,23 @@ import type { PlasmoMessaging } from "@plasmohq/messaging"
 import { getChromeCurrentTab } from "~utils/chromeMethods"
 
 const handler: PlasmoMessaging.PortHandler = async (req, res) => {
-  chrome.tabs.create({ url: req.body.env.value })
-  // chrome.tabs.update(tabId, { url: req.body.env.value }).then((result) => {
-  //   chrome.webNavigation.onCompleted.addListener(
-  //     function onCompleted(details) {
-  //       if (details.tabId === tabId) {
-  //         res.send({
-  //           status: "success",
-  //           tabId
-  //         })
-  //         const timerId = setTimeout(() => {
-  //           chrome.webNavigation.onCompleted.removeListener(onCompleted)
-  //           clearTimeout(timerId)
-  //         }, 5000)
-  //       }
-  //     },
-  //     { url: [{ urlMatches: req.body.env.value }] }
-  //   )
-  // })
+  chrome.tabs.create({ url: req.body.env.value }, (tab) => {
+    chrome.webNavigation.onCompleted.addListener(
+      function onCompleted(details) {
+        if (details.tabId === tab.id) {
+          chrome.tabs.sendMessage(tab.id, {
+            action: "login",
+            ...req.body
+          })
+          const timerId = setTimeout(() => {
+            chrome.webNavigation.onCompleted.removeListener(onCompleted)
+            clearTimeout(timerId)
+          }, 5000)
+        }
+      },
+      { url: [{ urlMatches: req.body.env.value }] }
+    )
+  })
 }
 
 export default handler
